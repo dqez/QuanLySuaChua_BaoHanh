4f4b7b273d1e15e37ca2dd8b920949703b53b783
Tranhuynhvu <tranhuynhvu2k4@gmail.com>
2025-05-14 20:50:16 +0700

    Thêm UrlHinhAnh cho bảng Sản phẩm

---
+++ Migrations/20250514134938_add_UrlHinhAnh_TableSanPham.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuanLySuaChua_BaoHanh.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_UrlHinhAnh_TableSanPham : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UrlHinhAnh",
+                table: "SanPham",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UrlHinhAnh",
+                table: "SanPham");
+        }
+    }
+}
