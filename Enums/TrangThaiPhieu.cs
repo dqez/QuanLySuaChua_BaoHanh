15f21b8b6a8724755d638888634230eb3c69721d
Zeq <124875024+ZEQ-10@users.noreply.github.com>
2025-05-15 23:34:23 +0700

    edit enum/trangthaiphieu

--- Enums/TrangThaiPhieu.cs
+++ Enums/TrangThaiPhieu.cs
@@ -4,18 +4,48 @@ namespace QuanLySuaChua_BaoHanh.Enums
 {
     public enum TrangThaiPhieu
     {
-        [Display(Name = "Chờ xác nhận")]
+        //Khachhang đăng ký phiếu sửa chữa
+        [Display(Name = "Chờ xác nhận")] 
         ChoXacNhan,
+        
+        //tư vấn viên xác nhận
+        [Display(Name = "Đã xác nhận")]
+        DaXacNhan,
+        
+        //quản trị viên phân công (KythuatID) 
+        [Display(Name = "Đã phân công")] 
+        DaPhanCong,
 
-        [Display(Name = "Đã tiếp nhận")]
-        DaTiepNhan,
+        //kỹ thuât viên chọn ngày hẹn
+        [Display(Name = "Chờ kiểm tra")]
+        ChoKiemTra,
 
+        //kythuatvien xác nhận, không -> đã hủy
+        [Display(Name = "Khách đồng ý sửa")] 
+        KhachDongY,
+        
+        //ky thuat viên cập nhật linh kiện 
         [Display(Name = "Đang sửa chữa")]
         DangSuaChua,
+        
+        //ky thuât viên cập nhật trạng thái sửa chữa
+        [Display(Name = "Đã sửa xong")] 
+        DaSuaXong,
 
+        //khachhang thanh toán.
+        [Display(Name = "Đã thanh toán")]
+        DaThanhToan,
+
+        //Quản trị viên cập nhật sau khi đóng gói và giao cho bên vận chuyển.
+        [Display(Name = "Đang vận chuyển")]
+        DangVanChuyen,
+
+        //khachhnag xác nhận nhận hàng.
         [Display(Name = "Hoàn thành")]
         HoanThanh,
 
+
+        //Khachhnag không đồng ý -> KTV cập nhật đã hủy.
         [Display(Name = "Đã hủy")]
         DaHuy
     }
